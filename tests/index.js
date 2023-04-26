a14fd0afaac5f42069337718c85fa38dac28e8ac
julienperronbloomays <julien.perron@bloomays.com>
2023-04-26 12:16:06 +0200

    commit for tests with fakes profiles

---
+++ tests/index.js
@@ -0,0 +1,21 @@
+(async () => {
+    const linkedins = 
+    [
+        'https://www.linkedin.com/in/lina-morel-9b1b7b1b8/',
+        'https://www.linkedin.com/in/rosa-barbet-69a6bb220/',
+        'https://www.linkedin.com/in/aurore-veneto-5a6034213/'
+    ];
+    for (let i = 0 ; i < linkedins.length ; i++){
+        const scrapedin = require('../src/scrapedin');
+        const fs = require('fs');
+        const cookies = fs.readFileSync(__dirname+'/cookie.json');
+        const options = {
+            cookies: JSON.parse(cookies),
+            isHeadless: false,
+            hasToLog: true,
+        };
+        const profileScraper = await scrapedin(options);
+        const profile = await profileScraper(linkedins[i]);
+        console.log(profile);
+    }
+})()
