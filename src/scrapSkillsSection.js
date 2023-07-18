b08f187ad1533c533a02165c99c444f911341435
Loïc Calvy <loic.calvy@bloomays.com>
2023-07-18 16:11:02 +0200

    direct go to skills

--- src/scrapSkillsSection.js
+++ src/scrapSkillsSection.js
@@ -1,9 +1,9 @@
 module.exports = async (page, section, url) => {
-  await page.goto(url, {waitUntil: 'domcontentloaded',});
-  await page.waitForXPath(section.selector);
+  await page.goto(`${url}/details/skills/`, {waitUntil: 'domcontentloaded',});
+  /*await page.waitForXPath(section.selector);
   const elements = await page.$x(section.selector);
   if (elements.length > 0){
-    await Promise.all([page.waitForNavigation({timeout: 5000}), elements[0].click()]);
+    await Promise.all([page.waitForNavigation({timeout: 5000}), elements[0].click()]);*/
     await page.waitForSelector('.pvs-list');
   
       skills = await page.evaluate(() =>
@@ -18,8 +18,8 @@ module.exports = async (page, section, url) => {
         }
       )
     ); 
-  } else {
+  /*} else {
     skills = [];
-  };
+  };*/
   return skills;
 };
